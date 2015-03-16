5d98f5bb12ba3394b290df85490f627b341dc516
Davin Ahn <davin.ahn@ridicorp.com>
2015-03-16 11:20:46 +0900

    메시지 및 코드 정리 // 설정 파일 추가

--- epub-validator.js
+++ epub-validator.js
@@ -9,27 +9,32 @@ var ps    = process,
     uuid  = require('node-uuid'),
     pkg   = require(path.join(__dirname, 'package.json')),
     cmd   = require('commander'),
-    rimraf= require('rimraf'),
+    rimraf= require('rimraf'), /*폴더 삭제용*/
     Zip   = require('adm-zip'),
     Epub  = require('./lib/EPubValidator'),
     File  = require('./lib/FileValidator'),
+    config= require('./lib/Config'),
     debug = require('debug')('app');
 
 var temp = 'epub-validator-temp';
 var unzipPath = path.join(temp, uuid.v1());
 
-var exit = function(code) {
+var exit = function(/*Number*/code) {
   try {
     rimraf.sync(unzipPath);
-  } catch(e) {}
-  
-  report.add(code ? 'APP-004' : 'APP-005', null, [file]);
+  } catch(e) {
+    report.add('APP-301', null, [unzipPath]);
+    debug(e);
+  }
+
+  report.add(code ? 'APP-102'/*검사 중지*/ : 'APP-103'/*검사 완료*/, null, [file]);
   report.print();
 
   debug('exit');
   ps.exit(code);
 };
 
+// TODO: 추후 옵션을 추가하거나 사용법을 구체적으로 추가해보자
 cmd.version(pkg.version)
    .usage('<file>')
    .parse(ps.argv);
@@ -38,18 +43,18 @@ debug('init');
 
 var file = cmd.args[0];
 if (file === undefined) {
-  report.add('APP-001');
+  report.add('APP-401'/*파라메터 부족*/);
   exit(1);
 }
 
 debug('check args');
 
 if (fs.existsSync(file) === false) {
-  report.add('APP-002', null, [file]);
+  report.add('APP-402'/*검사 대상을 찾을 수 없음*/, null, [file]);
   exit(2);
 }
 
-report.add('APP-003', null, [file]);
+report.add('APP-101'/*검사 시작*/, null, [file]);
 
 debug('check exists file');
 
@@ -57,7 +62,7 @@ try {
   var zip = new Zip(file);
   zip.extractAllTo(unzipPath, true);
 } catch(e) {
-  report.add('APP-006', null, [file]);
+  report.add('APP-403'/*압축해제 오류*/, null, [file]);
   exit(3);
 }
 
